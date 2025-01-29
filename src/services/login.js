91647093cb5434583302163dcfd5a3d5403e8934
Jackson Magnabosco <jackson.magnabosco@globalsys.com.br>
2025-01-29 12:57:52 -0300

    Adicionando autenticação

--- src/services/login.js
+++ src/services/login.js
@@ -1,6 +1,6 @@
 export const login = async ({ email, password }) => {
     try {
-      const response = await fetch("http://localhost:8000/auth/login", {
+      const response = await fetch("https://death-star.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
